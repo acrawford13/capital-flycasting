67f1c1bf00e0f314976138b57864fe0ca81dcac2
Andrea Crawford <andrea.crawford13@gmail.com>
2018-01-18 12:30:50 +0100

    Add wood textured header

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,3 +4,6 @@ import watch from 'gulp-watch';
 
 
 gulp.task('watch', ()=>{return watch('scss/*.scss').pipe(sass()).pipe(gulp.dest('css'))});
+gulp.task('js:dev', ()=>{
+    return gulp.src(['node_modules/jquery-parallax.js/parallax.min.js']).pipe(gulp.dest('js'));
+});
