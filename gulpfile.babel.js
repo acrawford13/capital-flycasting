983c799c3a265df8d32944e9d2c452ccbacd5e87
Andrea Crawford <andrea.crawford13@gmail.com>
2018-01-24 16:49:33 +0100

    Update/clean up packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,15 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import watch from 'gulp-watch';
+import autoprefixer from 'gulp-autoprefixer'
 
+gulp.task('watch', ()=>{return watch('scss/*.scss')
+                                .pipe(sass())
+                                .pipe(autoprefixer())
+                                .pipe(gulp.dest('css'))});
 
-gulp.task('watch', ()=>{return watch('scss/*.scss').pipe(sass()).pipe(gulp.dest('css'))});
 gulp.task('js:dev', ()=>{
-    return gulp.src(['node_modules/jquery-parallax.js/parallax.min.js']).pipe(gulp.dest('js'));
+    return gulp.src(['']).pipe(gulp.dest('js'));
 });
 gulp.task('css:dev', ()=>{
     return gulp.src(['grids-responsive*-min.css', 'pure-min.css'], {cwd: 'node_modules/purecss/build/'}).pipe(gulp.dest('css/purecss/'));
