838036c2d45056661f8a3e4f90d67005798dd74c
Andrea Crawford <andrea.crawford13@gmail.com>
2018-02-13 19:42:37 +0100

    Change gulp watch to include modular files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,12 @@ import sass from 'gulp-sass';
 import watch from 'gulp-watch';
 import autoprefixer from 'gulp-autoprefixer'
 
-gulp.task('watch', ()=>{return watch('scss/*.scss')
-                                .pipe(sass())
-                                .pipe(autoprefixer())
-                                .pipe(gulp.dest('css'))});
+gulp.task('watch', ()=>{return watch('scss/**/*.scss', function(){
+    gulp.src('scss/main.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('css'))});
+})
 
 gulp.task('js:dev', ()=>{
     return gulp.src(['']).pipe(gulp.dest('js'));
