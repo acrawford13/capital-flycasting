dc9deaa948348714a430dbe97c212b7766b4f25d
Andrea Crawford <andrea.crawford13@gmail.com>
2018-02-16 22:28:17 +0100

    Add js watch to gulpfile and enable verbose output

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,11 +4,21 @@ import watch from 'gulp-watch';
 import autoprefixer from 'gulp-autoprefixer';
 import babel from 'gulp-babel';
 
-gulp.task('watch', ()=>{return watch('scss/**/*.scss', function(){
-    gulp.src('scss/main.scss')
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('css'))});
+gulp.task('watch', ()=>{
+    watch('scss/**/*.scss', {verbose: true}, function(){
+        gulp.src('scss/main.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(gulp.dest('css'))
+    });
+
+    watch('src-js/*.js', {verbose: true}, function(){
+        gulp.src('src-js/app.js')
+               .pipe(babel({
+                   presets: ['env'],
+               }))
+               .pipe(gulp.dest('js'));
+    });
 })
 
 gulp.task('js:dev', ()=>{
