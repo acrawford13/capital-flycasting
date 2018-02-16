31b50464e44e075c51abe36a7327a0eade639caa
Andrea Crawford <andrea.crawford13@gmail.com>
2018-02-16 22:19:55 +0100

    Move app scripts to separate file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,8 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import watch from 'gulp-watch';
-import autoprefixer from 'gulp-autoprefixer'
+import autoprefixer from 'gulp-autoprefixer';
+import babel from 'gulp-babel';
 
 gulp.task('watch', ()=>{return watch('scss/**/*.scss', function(){
     gulp.src('scss/main.scss')
@@ -11,8 +12,13 @@ gulp.task('watch', ()=>{return watch('scss/**/*.scss', function(){
 })
 
 gulp.task('js:dev', ()=>{
-    return gulp.src(['']).pipe(gulp.dest('js'));
+    return gulp.src('src-js/app.js')
+               .pipe(babel({
+                   presets: ['env'],
+               }))
+               .pipe(gulp.dest('js'));
 });
+
 gulp.task('css:dev', ()=>{
     return gulp.src(['grids-responsive*-min.css', 'pure-min.css'], {cwd: 'node_modules/purecss/build/'}).pipe(gulp.dest('css/purecss/'));
 });
---
+++ src-js/app.js
@@ -0,0 +1,126 @@
+let selectedImage = null;
+
+function openImage(imageElement){
+    imageElement.clone().addClass('photo--temp').insertAfter(imageElement.parent());
+    imageElement.addClass('photo--clicked');
+    imageElement.parent('.photo-container').addClass('photo-container--clicked');
+}
+
+function closeImage(){
+    $('.photo--clicked').removeClass('photo--clicked');
+    $('.photo-container--clicked').removeClass('photo-container--clicked');
+    $('.photo--temp').remove();
+}
+
+function closeGalleryImage(){
+    $('.gallery__modal').css({'display':'none'});
+    removeKeyboardListener();
+}
+
+function showModal(element){
+    setModalImage(element);
+    $('.gallery__modal .modal__image').load(() => {
+        $('.gallery__modal').css({'display':'flex'});
+    });
+    addKeyboardListener();
+}
+
+function setModalImage(element){
+    selectedImage = element;
+    const caption = element.find('.photo__caption').html();
+    const imageNum = element.attr('data-index') + "/" + $('.gallery__item').length;
+    $('.gallery__modal .modal__image').attr('src', element.attr('data-src'));
+    $('.gallery__modal .modal__image').load(() => {
+        $('.gallery__modal .modal__caption').html(caption);
+        $('.gallery__modal .modal__imagenum').html(imageNum);
+    });
+}
+
+function nextGalleryImage(){
+    const nextImage = selectedImage.next('.gallery__item');
+    if(nextImage.length === 1){
+        selectedImage = nextImage;
+    } else {
+        selectedImage = selectedImage.siblings('.gallery__item').first();
+    }
+    setModalImage(selectedImage);
+}
+
+function prevGalleryImage(){
+    const prevImage = selectedImage.prev('.gallery__item');
+    if(prevImage.length === 1){
+        selectedImage = prevImage;
+    } else {
+        selectedImage = selectedImage.siblings('.gallery__item').last();
+    }
+    setModalImage(selectedImage);
+}
+
+function handleKeyboardInput(event){
+    switch(event.keyCode) {
+        case 27:
+            closeGalleryImage();
+            break;
+        case 39:
+            nextGalleryImage();
+            break;
+        case 37:
+            prevGalleryImage();
+            break;
+        default:
+            break;
+    }
+}
+
+function addKeyboardListener(){
+    $(document).on('keyup', handleKeyboardInput);
+}
+
+function removeKeyboardListener(){
+    $(document).off('keyup', handleKeyboardInput);
+}
+
+
+$(document).on('click', '.gallery__item', () => {
+    showModal($(this));
+});
+
+$(document).on('click', '.modal__image', (e) => {
+    e.stopPropagation();
+    nextGalleryImage();
+});
+
+$(document).on('click', '.gallery__modal', () => {
+    closeGalleryImage();
+});
+
+$(document).on('click', '.modal__caption', (e) => {
+    e.stopPropagation();
+});
+
+$(document).on('click', '.modal__close', (e) => {
+    closeGalleryImage();
+});
+
+$(document).on('click', '.modal__arrow--next', (e) => {
+    e.stopPropagation();
+    nextGalleryImage();
+});
+
+$(document).on('click', '.modal__arrow--prev', (e) => {
+    e.stopPropagation();
+    prevGalleryImage();
+});
+
+$(document).on('click', '.photo--clickable', () => {
+    closeImage();
+    openImage($(this));
+});
+
+$(document).on('click', '.photo-container--clicked', () => {
+    closeImage();
+});
+
+$(document).on('click', '.photo--clicked', () => {
+    closeImage();
+});
