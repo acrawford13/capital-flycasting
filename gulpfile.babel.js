25f88eb2ca60f721aef7eea1500f898a910630e2
Andrea Crawford <andrea.crawford13@gmail.com>
2018-02-17 13:51:50 +0100

    Change folder structure and gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,34 +5,95 @@ import autoprefixer from 'gulp-autoprefixer';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
 import pump from 'pump';
+import htmlreplace from 'gulp-html-replace';
+
+const devSCSS = () => {
+    gulp.src('src/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('css'));
+};
+
+const devJS = () => {
+    gulp.src('src/js/app.js')
+            .pipe(babel({
+                presets: ['env'],
+            }))
+            .pipe(gulp.dest('js'));
+};
+
+const devTemplates = () => {
+    gulp.src('src/templates/**/')
+        .pipe(gulp.dest('templates'));
+};
+
+const buildSCSS = () => {
+    gulp.src('src/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('css'));
+};
+
+const buildJS = () => {
+    console.log('building javascript');
+    gulp.src('src/js/app.js')
+        .pipe(babel({
+            presets: ['env'],
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
+};
+
+const buildTemplates = () => {
+    gulp.src('src/templates/**/')
+        .pipe(htmlreplace({ js: "{% do assets.addJs('theme://js/app.min.js', 100) %}"}))
+        .pipe(gulp.dest('templates'));
+};
+
+const buildDevFiles = ()=>{
+    devSCSS();
+    devJS();
+    devTemplates();
+};
 
 gulp.task('watch', ()=>{
-    watch('scss/**/*.scss', {verbose: true}, function(){
-        gulp.src('scss/main.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(autoprefixer())
-            .pipe(gulp.dest('css'))
+    buildDevFiles();
+
+    watch('src/scss/**/*.scss', {verbose: true}, function(){
+        devSCSS();
     });
 
-    watch('src-js/*.js', {verbose: true}, function(){
-        gulp.src('src-js/app.js')
-               .pipe(babel({
-                   presets: ['env'],
-               }))
-               .pipe(gulp.dest('js'));
+    watch('src/js/**/*.js', {verbose: true}, function(){
+        devJS();
     });
-})
+
+    watch('src/templates/**/', {verbose: true}, function(){
+        devTemplates();
+    });
+});
+
+gulp.task('build:dev', buildDevFiles);
+
+gulp.task('build:dist', () => {
+   buildSCSS();
+   buildJS();
+   buildTemplates();
+});
 
 gulp.task('js:build', (cb)=>{
     pump([
-        gulp.src('src-js/app.js'),
+        gulp.src('src/templates/base.html.twig'),
+        htmlreplace({ js: "{% do assets.addJs('theme://js/app.min.js', 100) %}"}),
+        gulp.dest('templates'),
+
+        gulp.src('src/js/app.js'),
         babel({
             presets: ['env']
         }),
         uglify(),
         gulp.dest('js')
     ],
-    cb)
+    cb);
 });
 
 gulp.task('css:dev', ()=>{
