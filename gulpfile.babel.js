2440b7ea97d97ac95767bd6fbd88c6e964b762e2
Andrea Crawford <andrea.crawford13@gmail.com>
2019-02-24 18:04:14 +0100

    Update gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-import gulp from 'gulp';
+import { src, dest, task, parallel, series } from 'gulp';
 import sass from 'gulp-sass';
 import watch from 'gulp-watch';
 import autoprefixer from 'gulp-autoprefixer';
@@ -7,84 +7,83 @@ import uglify from 'gulp-uglify';
 import htmlreplace from 'gulp-html-replace';
 import rename from 'gulp-rename';
 
-const devSCSS = () => {
-    gulp.src('src/scss/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('css'));
-};
+const defaultDest = '';
 
-const devJS = () => {
-    gulp.src('src/js/app.js')
-        .pipe(babel({
-            presets: ['env'],
-        }))
-        .pipe(gulp.dest('js'));
-};
+const getConsoleArg = key => process.argv.find(value => value.match(key)).split(':')[1];
 
-const devTemplates = () => {
-    gulp.src('src/templates/**/')
-        .pipe(gulp.dest('templates'));
-};
+const devSCSS = ({ savePath }) => src('src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(dest(`${savePath}/css`));
 
-const buildSCSS = () => {
-    gulp.src('src/scss/main.scss')
-        .pipe(autoprefixer())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('css'));
-};
+const devJS = ({ savePath }) => src('src/js/app.js')
+    .pipe(babel({
+        presets: ['env'],
+    }))
+    .pipe(dest(`${savePath}/js`));
 
-const buildJS = () => {
-    gulp.src('src/js/app.js')
-        .pipe(babel({
-            presets: ['env'],
-        }))
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('js'));
-};
+const devTemplates = ({ savePath }) => src('src/templates/**/')
+    .pipe(dest(`${savePath}/templates`));
 
-const buildTemplates = () => {
-    gulp.src('src/templates/**/')
-        .pipe(htmlreplace({
-            js: "{% do assets.addJs('theme://js/app.min.js', 100) %}",
-            keepBlockTags: false
-        }))
-        .pipe(gulp.dest('templates'));
-};
+const buildSCSS = () => src('src/scss/main.scss')
+    .pipe(autoprefixer())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(dest('css'));
 
-gulp.task('watch', ['build:dev'], ()=>{
+const buildJS = () => src('src/js/app.js')
+    .pipe(babel({
+        presets: ['env'],
+    }))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(dest('js'));
+
+const buildTemplates = () => src('src/templates/**/')
+    .pipe(htmlreplace({
+        js: '{% do assets.addJs(\'theme://js/app.min.js\', 100) %}',
+        keepBlockTags: false
+    }))
+    .pipe(dest('templates'));
+
+
+task('build:dev', ()=>{
+    const savePath = getConsoleArg('dest') || defaultDest;
+    devSCSS({ savePath });
+    devJS({ savePath });
+    devTemplates({ savePath });
+});
+
+const buildDev = task('build:dev');
+
+task('watch', parallel([buildDev], function watching(){
+    const savePath = getConsoleArg('dest') || defaultDest;
     watch('src/scss/**/*.scss', {verbose: true}, function(){
-        devSCSS();
+        devSCSS({ savePath });
     });
 
     watch('src/js/**/*.js', {verbose: true}, function(){
-        devJS();
+        devJS({ savePath });
     });
 
     watch('src/templates/**/', {verbose: true}, function(){
-        devTemplates();
+        devTemplates({ savePath });
     });
-});
+}));
 
-gulp.task('build:dev', ()=>{
-    devSCSS();
-    devJS();
-    devTemplates();
-});
+task('build', parallel(
+    buildSCSS,
+    buildJS,
+    buildTemplates
+));
 
-gulp.task('build', () => {
-    buildSCSS();
-    buildJS();
-    buildTemplates();
-});
+const build = task('build');
 
-gulp.task('build:export', ['build'], ()=> {
-    gulp.src('./js/**', { base: '.' }).pipe(gulp.dest('export'));
-    gulp.src('./css/**', { base: '.' }).pipe(gulp.dest('export'));
-    gulp.src('./images/**', { base: '.' }).pipe(gulp.dest('export'));
-    gulp.src('./blueprints/**', { base: '.' }).pipe(gulp.dest('export'));
-    gulp.src('./templates/**', { base: '.' }).pipe(gulp.dest('export'));
-    gulp.src('blueprints.yaml').pipe(gulp.dest('export'));
-    gulp.src('capitalflycasting.yaml').pipe(gulp.dest('export'));
-});
+task('build:export', series(build, ()=> {
+    src('./js/**', { base: '.' }).pipe(dest('export'));
+    src('./css/**', { base: '.' }).pipe(dest('export'));
+    src('./images/**', { base: '.' }).pipe(dest('export'));
+    src('./blueprints/**', { base: '.' }).pipe(dest('export'));
+    src('./templates/**', { base: '.' }).pipe(dest('export'));
+    src('blueprints.yaml').pipe(dest('export'));
+    src('capitalflycasting.yaml').pipe(dest('export'));
+}));
