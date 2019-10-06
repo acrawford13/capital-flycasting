2e305c8c2ff1ad38fa887cc4567d440dabe23cc2
Andrea Crawford <andrea.crawford13@gmail.com>
2019-10-06 12:57:21 +0100

    Update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+/* eslint-disable no-console */
+/* eslint-env node */
+
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const htmlreplace = require('gulp-html-replace');
+const rename = require('gulp-rename');
+
+const basePath = '/var/www/html/capital-flycasting/user/themes/capital-flycasting/';
+
+function devSCSS(cb) {
+    src('src/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(dest(`${basePath}css`));
+    cb();
+}
+
+function devJS(cb) {
+    src('src/js/app.js')
+        .pipe(
+            babel({
+                presets: ['env']
+            })
+        )
+        .pipe(dest(`${basePath}js`));
+    cb();
+}
+
+function devTemplates(cb) {
+    src('src/templates/**/*').pipe(dest(`${basePath}templates`));
+    cb();
+}
+
+function buildSCSS(cb) {
+    src('src/scss/main.scss')
+        .pipe(autoprefixer())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(dest('css'));
+    cb();
+}
+
+function buildJS(cb) {
+    src('src/js/app.js')
+        .pipe(
+            babel({
+                presets: ['env']
+            })
+        )
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('js'));
+    cb();
+}
+
+function buildTemplates(cb) {
+    src('src/templates/**/')
+        .pipe(
+            htmlreplace({
+                js: '{% do assets.addJs(\'theme://js/app.min.js\', 100) %}',
+                keepBlockTags: false
+            })
+        )
+        .pipe(dest('templates'));
+    cb();
+}
+
+function buildDev(cb) {
+    series(devSCSS, devJS, devTemplates);
+    cb();
+}
+
+function devBuildTasks(cb) {
+    watch('src/scss/**/*.scss', devSCSS);
+    watch('src/js/**/*.js', devJS),
+    watch('src/templates/**/*', devTemplates);
+    cb();
+}
+
+function build(cb) {
+    series(buildSCSS, buildJS, buildTemplates);
+    cb();
+}
+
+function buildExport(cb) {
+    src('./js/**', { base: '.' }).pipe(dest(`${basePath}export`));
+    src('./css/**', { base: '.' }).pipe(dest(`${basePath}export`));
+    src('./images/**', { base: '.' }).pipe(dest(`${basePath}export`));
+    src('./blueprints/**', { base: '.' }).pipe(dest(`${basePath}export`));
+    src('./templates/**', { base: '.' }).pipe(dest(`${basePath}export`));
+    src('blueprints.yaml').pipe(dest(`${basePath}export`));
+    src('capitalflycasting.yaml').pipe(dest(`${basePath}export`));
+    cb();
+}
+
+
+
+exports['build:dev'] = buildDev;
+exports.build = build;
+exports['build:export'] = series(build, buildExport);
+exports.watch = series(buildDev, devBuildTasks);
